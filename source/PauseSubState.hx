9e5f37fc0b9451a644fe23be993b078ffc14c29f
Amogus124 <onixorochinho@gmail.com>
2022-09-09 02:25:05 -0400

    vai se fuder eu achei que essa bosta tava compilando

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -237,7 +237,7 @@ class PauseSubState extends MusicBeatSubstate
 			}
 		}
 
-		if (controls.J)
+		if (FlxG.keys.justPressed.J)
 		{
 			// for reference later!
 			// PlayerSettings.player1.controls.replaceBinding(Control.LEFT, Keys, FlxKey.J, null);
